f7df6f18417d766b45ae72538f3199b6974b989b
Katryel Menezes Vieira <katryelmenezesvieira14@gmail.com>
2021-03-18 16:40:22 -0300

    Projeto Iniciado

---
+++ resources/views/layout/script.blade.php
@@ -0,0 +1,8 @@
+<script>
+    <script src="{{ asset('script/jquery.js') }}"></script>
+    <script src="{{ asset('script/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('script/jquery.mask.min.js') }}"></script>
+    <script src="{{ asset('script/sweetalert2.js') }}"></script>
+    <script src="{{ asset('script/bundle.js') }}"></script>
+    <script src="{{ asset('script/main.js') }}"></script>
+</script>
---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+@include('layout.head')
+<body>
+
+@include('layout.script')
+</body>
+</html>
